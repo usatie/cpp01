8c8149b7b084521d7f00bb0681fb4f88791aa4e9
Shun Usami <usatie@gmail.com>
2023-03-27 11:13:26 +0900

    :bug: ex01 Handle negative N

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -2,11 +2,37 @@
 
 int	main(void)
 {
+	// Normal Case
 	{
+		int N = 5 ;
 		Zombie *zombies;
 
-		zombies = zombieHorde(5, "Michale") ;
-		for ( int i = 0; i < 5; i++ )
+		zombies = zombieHorde(N, "Michale") ;
+		for ( int i = 0; i < N; i++ )
+		{
+			zombies[i].announce() ;
+		}
+		delete[] zombies ;
+	}
+	// N is zero
+	{
+		int N = 0 ;
+		Zombie *zombies;
+
+		zombies = zombieHorde(N, "Michale") ;
+		for ( int i = 0; i < N; i++ )
+		{
+			zombies[i].announce() ;
+		}
+		delete[] zombies ;
+	}
+	// N is negative
+	{
+		int N = -1 ;
+		Zombie *zombies;
+
+		zombies = zombieHorde(N, "Michale") ;
+		for ( int i = 0; i < N; i++ )
 		{
 			zombies[i].announce() ;
 		}
--- ex01/src/zombieHorde.cpp
+++ ex01/src/zombieHorde.cpp
@@ -4,6 +4,8 @@ Zombie*	zombieHorde( int N, std::string name )
 {
 	Zombie	*zombies ;
 
+	if ( N <= 0 )
+		return NULL ;
 	zombies = new Zombie[N] ;
 	for ( int i = 0 ; i < N ; i++ )
 	{
